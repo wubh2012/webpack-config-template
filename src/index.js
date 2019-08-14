df2e9b6c50f8403b94a41cb88cfc457d881b0e55
wubh2012 <wubh2012@163.com>
2019-08-14 12:15:54 +0800

    支持 css, scss, 文件 loader

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+import './style.css'
+import './main.scss'
 
 function component() {
   let element = document.createElement('div');
-
+  element.className = 'hello';
   element.innerHTML = ['Hello', 'webpack'].join(' ');
 
   return element;
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,25 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [{
+        test: /\.css|.scss|.sass$/,
+        use: [
+          'style-loader', 'css-loader','sass-loader',
+        ]
+      },      
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      }
+    ]
   }
 };
