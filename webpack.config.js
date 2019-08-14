5e864478eef057c6b4dccb2f736955e0c832a750
wubh2012 <wubh2012@163.com>
2019-08-14 16:34:39 +0800

    使用babel将代码转成es5

--- webpack.config.js
+++ webpack.config.js
@@ -41,7 +41,18 @@ module.exports = env => {
           use: [
             'file-loader'
           ]
-        }
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-transform-runtime']
+            }
+          }
+        }      
       ]
     },
     plugins: [
